3faff20307358a712c69476208c45f9776cf3537
BotchaVarun <varunbotcha@gmail.com>
2025-04-09 20:35:16 +0530

    frontend commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route
-            path="/Course"
+            path="/course"
             element={ <Courses />}
           />
           <Route path="/signup" element={<Signup />} />
